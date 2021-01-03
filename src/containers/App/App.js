edf48d2cef64a7c8cac84dbb5af27a52653c2ce5
justinwasserstein <justin.wasserstein@gmail.com>
2021-01-03 18:24:41 -0500

    Add ProfilePage

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,10 +4,12 @@ import Footer from '../Footer';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {configureStore} from '../../store';
+import withAuth from '../../hocs/withAuth';
 import LandingPage from '../LandingPage';
 import AboutPage from '../AboutPage';
 import LoginPage from '../LoginPage';
 import SignupPage from '../SignupPage';
+import ProfilePage from '../ProfilePage';
 
 const store = configureStore();
 
@@ -18,6 +20,7 @@ function App() {
                 <div className="App">
                     <Navbar />
                     <Switch>
+                        <Route path='/profile' component={withAuth(ProfilePage)} />
                         <Route path='/signup' component={SignupPage} />
                         <Route path='/login' component={LoginPage} />
                         <Route path='/about' component={AboutPage} />
