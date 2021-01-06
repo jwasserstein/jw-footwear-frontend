e19fd5b15a5858b453cc1887148e1876445c652d
justinwasserstein <justin.wasserstein@gmail.com>
2021-01-06 15:13:33 -0500

    Remove callback functions and promises from cart action creators because they don't need to be async

--- src/store/actions/cart.js
+++ src/store/actions/cart.js
@@ -1,66 +1,39 @@
 import {ADD_CART_ITEM, RESTORE_CART, REMOVE_CART_ITEM} from '../actionTypes';
 
 export function addCartItem(id, size, quantity) {
-	return dispatch => {
-		return new Promise(async (resolve, reject) => {
-			try {
-                let lsCart = JSON.parse(localStorage.getItem('cart'));
-                if(!lsCart){
-                    lsCart = [{id, size, quantity}]
-				} else {
-					const idSizeCombo = lsCart.find(item => item.id === id && item.size === size);
-					if(idSizeCombo){
-						idSizeCombo.quantity += quantity;
-					} else {
-						lsCart.push({id, size, quantity});
-					}
-				}
-                localStorage.setItem('cart', JSON.stringify(lsCart));
-
-                const lsCartCount = +localStorage.getItem('cartCount') || 0;
-                localStorage.setItem('cartCount', lsCartCount + quantity);
-
-				dispatch({type: ADD_CART_ITEM, id, size, quantity});
-				return resolve();
-			} catch(err) {
-				return reject(err.message);
-			}
-		});
+	let lsCart = JSON.parse(localStorage.getItem('cart'));
+	if(!lsCart){
+		lsCart = [{id, size, quantity}]
+	} else {
+		const idSizeCombo = lsCart.find(item => item.id === id && item.size === size);
+		if(idSizeCombo){
+			idSizeCombo.quantity += quantity;
+		} else {
+			lsCart.push({id, size, quantity});
+		}
 	}
+	localStorage.setItem('cart', JSON.stringify(lsCart));
+
+	const lsCartCount = +localStorage.getItem('cartCount') || 0;
+	localStorage.setItem('cartCount', lsCartCount + quantity);
+
+	return {type: ADD_CART_ITEM, id, size, quantity};
 }
 
 export function restoreCart(){
-    return dispatch => {
-		return new Promise(async (resolve, reject) => {
-			try {
-                const lsCart = JSON.parse(localStorage.getItem('cart')) || [];
-                const lsCartCount = +localStorage.getItem('cartCount') || 0;
-				dispatch({type: RESTORE_CART, lsCart, lsCartCount});
-				return resolve();
-			} catch(err) {
-				return reject(err.message);
-			}
-		});
-	}
+	const lsCart = JSON.parse(localStorage.getItem('cart')) || [];
+	const lsCartCount = +localStorage.getItem('cartCount') || 0;
+	return {type: RESTORE_CART, lsCart, lsCartCount};
 }
 
 export function removeCartItem(id, size, quantity){
-	return dispatch => {
-		return new Promise(async (resolve, reject) => {
-			try {
-				let lsCart = JSON.parse(localStorage.getItem('cart')) || [];
-				let lsCartCount = +localStorage.getItem('cartCount') || 0;
+	let lsCart = JSON.parse(localStorage.getItem('cart')) || [];
+	let lsCartCount = +localStorage.getItem('cartCount') || 0;
 
-				lsCart = lsCart.filter(item => item.id !== id || item.size !== size);
-				lsCartCount -= quantity;
+	lsCart = lsCart.filter(item => item.id !== id || item.size !== size);
+	lsCartCount -= quantity;
 
-				localStorage.setItem('cart', JSON.stringify(lsCart));
-				localStorage.setItem('cartCount', lsCartCount);
-				dispatch({type: REMOVE_CART_ITEM, id, size, quantity});
-				return resolve();
-			} catch (err) {
-				return reject(err.message);
-			}
-		});
-	}
+	localStorage.setItem('cart', JSON.stringify(lsCart));
+	localStorage.setItem('cartCount', lsCartCount);
+	return {type: REMOVE_CART_ITEM, id, size, quantity};
 }
--- todo.md
+++ todo.md
@@ -3,7 +3,6 @@
 - Make everything smaller.  Update Figma.  24px -> 18px, 18px -> 14px.
 - Package up common code between BOW and JW Footwear, share using an NPM package or Bit
 - Add provision for adding multiple items at once to the cart
-- Consider removing callback functions and promises from actions that don't need to be async
 
 # Code to Share
 - withAuth() in hocs/withAuth.js
