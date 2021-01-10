c114210fdec70c509ffaebb2501009a8d7c0b45c
justinwasserstein <justin.wasserstein@gmail.com>
2021-01-10 11:38:48 -0500

    Remove /api from end of REACT_APP_BACKEND_URL environment variable and replaced hero image reference to local image with reference to image on backend

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -8,7 +8,7 @@ const LandingPage = () => {
     return (
         <div className='LandingPage-main-container'>
             <main className='LandingPage-Hero'>
-                <div className='LandingPage-Hero-image'></div>
+                <div className='LandingPage-Hero-image' style={{backgroundImage: `url(${process.env.REACT_APP_BACKEND_URL + '/images/shoe-on-feet.jpg'})`}}></div>
                 <div className="LandingPage-Hero-text">
                     <h1>Welcome to JW Footwear!</h1>
                     <p>We’re the largest retailer of shoes that doesn’t actually sell shoes!</p>
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 export async function apiCall(type, url, data) {
 	if(type.toLowerCase() === 'get'){
-		const resp = await fetch(process.env.REACT_APP_BACKEND_URL + url, {
+		const resp = await fetch(process.env.REACT_APP_BACKEND_URL + '/api' + url, {
 			method: type,
 			headers: {
 				'Content-Type': 'application/json',
@@ -10,7 +10,7 @@ export async function apiCall(type, url, data) {
 		const d = await resp.json();
 		return d;
 	} else {
-		const resp = await fetch(process.env.REACT_APP_BACKEND_URL + url, {
+		const resp = await fetch(process.env.REACT_APP_BACKEND_URL + '/api' + url, {
 			method: type,
 			headers: {
 				'Content-Type': 'application/json',
