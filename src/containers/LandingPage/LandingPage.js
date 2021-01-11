aa4ae237764b1ef247dd75bbef964a4d9e238603
justinwasserstein <justin.wasserstein@gmail.com>
2021-01-11 05:53:30 -0500

    Add dynamic height for LandingPage-main-container based on window.innerHeight

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -1,22 +1,45 @@
-import React, {useEffect} from 'react';
+import React, {Component, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import './LandingPage.css';
 
-const LandingPage = () => {
-    useEffect(() => document.title = 'JW Footwear | Welcome');
+class LandingPage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            width: 0,
+            height: 0
+        };
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
 
-    return (
-        <div className='LandingPage-main-container'>
-            <main className='LandingPage-Hero'>
+    componentDidMount(){
+        document.title = 'JW Footwear | Welcome';
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
+    }
+
+    componentWillUnmount(){
+        window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+
+    updateWindowDimensions(){
+        this.setState({...this.state, width: window.innerWidth, height: window.innerHeight});
+    }
+
+    render() {
+        const {width, height} = this.state;
+        const divHeight = width > 900 ? height - 113 : height - 137;
+        return (
+            <div className='LandingPage-main-container' style={{height: divHeight}}>
                 <div className='LandingPage-Hero-image' style={{backgroundImage: `url('${process.env.REACT_APP_BACKEND_URL}/images/shoe-on-feet.jpg')`}}></div>
                 <div className="LandingPage-Hero-text">
                     <h1>Welcome to JW Footwear!</h1>
                     <p>We’re the largest retailer of shoes that doesn’t actually sell shoes!</p>
                     <Link to='/products'>Shop Now!</Link>
                 </div>
-            </main>
-        </div>
-    );
+            </div>
+        );
+    }
 };
 
 export default LandingPage;
